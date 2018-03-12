ddc205e986a635ed2fe4116794779b138f879d85
thareqyusuf <thareqmyha@gmail.com>
2018-03-12 15:33:13 +0700

    Update Ikan.h

--- Ikan.h
+++ Ikan.h
@@ -12,14 +12,21 @@ using namespace std;
 class Ikan
 : public Benda{
 protected:
+	//Waktu Ikan bergerak bebas setelah makan
 	const int fullTimer;
+	//Waktu Ikan mencari makanan
 	const int starveTimer;
-	const int producedCoin;
+	//Koin yang dihasilkan ikan
+	Koin producedCoin;
+	//Muka ikan menghadap kemana
 	int FaceDIrection;
+	//Jumlah makanan yang sudah dimakan
 	int countFood;
 	
 public:
+	// a untuk x, b untuk y, s untuk speed dan akan memanggil ctor Benda
 	Ikan(int a, int b, int s);
+	//
 	void eat(MakananIkan& m) = 0;
 	void catchFood();
 	void producedCOin() = 0;
